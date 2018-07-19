c6c5d1c0a80b85cfd6a09a65d1e107dd60cd1a7a
Kumar-Vignesh <36804156+Kumar-Vignesh@users.noreply.github.com>
2018-07-19 12:40:46 -0400

    Create fclose.c

---
+++ stdio/fclose.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void)
+{
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+    for( count=0; count <= 1000000; count++) {
+      int fflush( FILE *stream );
+    }
+   
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf(“fclose took %6.6f\n", secs);
+    
+}
