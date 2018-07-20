e8343e8cc854e589e047f3498b3d56621dc4a0cd
purplecode1 <33673760+purplecode1@users.noreply.github.com>
2018-07-20 11:19:21 -0400

    Create iswxdigit.c

---
+++ c_files/iswxdigit.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <wchar.h>
+#include <wctype.h>
+#include <locale.h>
+
+int main(void)
+{
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+    char digit = 0;
+
+    for( count=0; count <= 1000000; count++) 
+    {
+     iswxdigit(digit);
+      digit++;
+    }
+
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("iswxdigit took %6.6f\n", secs);
+}
