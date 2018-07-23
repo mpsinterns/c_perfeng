1f746e128daee6eeade7e5e0347c45578a609bc2
Swathi Parthibha <swathiparthibha@gmail.com>
2018-07-22 21:06:41 -0400

    Create strlen.c

---
+++ c_files/String.h/strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void)
+{
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+    const char str[] = "How many characters does this string contain?";
+    for( count=0; count <= 1000000; count++) {
+        int a = strlen(str);
+    }
+   
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("strlen took %6.9f\n", secs);
+    
+}
