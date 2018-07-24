71857e42a20883b558f7358656dd2dbac98e25df
purplecode1 <33673760+purplecode1@users.noreply.github.com>
2018-07-23 20:50:53 -0400

    Create wcscat.c

---
+++ c_files/wcscat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <wchar.h>
+#include <wctype.h>
+#include <locale.h>
+
+
+int main(void)
+{
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+    wchar_t str[50] = L"Земля, прощай.";
+
+    for( count=0; count <= 10; count++) 
+    {
+        wcscat(str, L" ");
+    }
+
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("wcscat took %6.6f\n", secs);
+}
