68b6fc5c29afa9dc664519bbd47bb72ac61a947c
Amey Venkatanarayan <amey.venkat@gmail.com>
2018-07-27 10:46:33 -0400

    Create acosh.c

---
+++ c_files/acosh.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(void)
+{
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    signed int count;
+    double x = 42;
+    for( count=0; count <= 1000000; count++) {
+        acosh(x);
+    }
+    
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("inverse hyperbolic cos took %6.9f\n", secs);
+    
+}
+
